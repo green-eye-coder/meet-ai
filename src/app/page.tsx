bfe40cd557f4d9fec769a49e008f199b3a7e94c0
Hruthik chavan D <hruthikchavandsmg@gmail.com>
2025-06-04 18:21:05 +0530

    feat: add social authentication with GitHub and Google, update sign-in and sign-up views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,18 @@
-"use client";//can you tell me what this does?
-// The "use client" directive indicates that this file is a client-side component in a Next.js application.
+import {auth} from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Button } from '@/components/ui/button';
-import { useState } from 'react';
-import {Input} from '@/components/ui/input';
-import { authClient } from "@/lib/auth-client";
+const Page= async ()=>{
+  const session=await auth.api.getSession({
+    headers: await headers(),
+  });
 
-export default function Home() {
-  const[name, setName] = useState('');
-  const[email, setEmail] = useState('');
-  const[password, setPassword] = useState('');
-
-  const {data: session} = authClient.useSession() 
-
-  const onSubmit=()=>{
-    authClient.signUp.email({
-      email,
-      name,
-      password,
-    },{
-      onError:(ctx)=>{
-        window.alert(ctx.error.message);
-        console.error("something went wrong");
-      },
-      onSuccess:()=>{
-        window.alert("User created successfully");
-        
-      }
-    })
-  }
-  const onLogin=()=>{
-    authClient.signIn.email({
-      email,
-      password,
-    },{
-      onError:(ctx)=>{
-        window.alert(ctx.error.message);
-        console.error("something went wrong");
-      },
-      onSuccess:()=>{
-        window.alert("User logged in successfully");
-        
-      }
-    })
+  if(!session){
+    redirect("/sign-in");
   }
 
-
-  
-  if(session){
-    return(
-      <div className='flex flex-col p-4 gap-y-4'>
-        <p className='text-2xl font-bold'>Welcome {session.user.name}</p>
-        <Button variant={'destructive'} onClick={()=>{authClient.signOut()}}>Sign out</Button>
-      </div>
-    )
-  }
-
-  return (
-    <div className='container'>
-    <div  className='SigUp flex flex-col items-center gap-y-4 p-4 m-3 w-1/3 rounded shadow-lg shadow-blue-400/50'>
-      <Input placeholder='Enter name' value={name} onChange={(e)=>setName(e.target.value)}/>
-      <Input placeholder='Enter email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-      <Input placeholder='Enter password' type='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-    <Button  className='m-5  shadow-lg shadow-blue-500/75' onClick={onSubmit}>submit</Button>
-    </div>
-
-    <div  className='SigUp flex flex-col items-center gap-y-4 p-4 m-3 w-1/3 rounded shadow-lg shadow-blue-400/50'>
-      <Input placeholder='Enter email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-      <Input placeholder='Enter password' type='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-    <Button  className='m-5  shadow-lg shadow-blue-500/75' onClick={onLogin} variant={'primary'}>Login</Button>
-    </div>
-    </div>
-  );
+  return <HomeView/>
 }
+
+export default Page;
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -4,7 +4,7 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { OctagonAlertIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
@@ -21,23 +21,24 @@ import { Alert, AlertTitle } from "@/components/ui/alert";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+import { FaGoogle,FaGithub } from "react-icons/fa";
 
 // Define validation schema for sign-in form using Zod
 const signInSchema = z.object({
   email: z.string().email("Invalid email address"),
-  password: z
-    .string()
-    .min(1, { message: "Password is required" }),
+  password: z.string().min(1, { message: "Password is required" }),
 });
 
 // SignInView component
 export const SignInView = () => {
   // Router for navigation after successful sign-in
-  const router = useRouter();
+
 
   // State for error messages and loading state
   const [error, setError] = useState<string | null>(null);
   const [pending, setPending] = useState<boolean>(false);
+  const router = useRouter();
 
   // Initialize react-hook-form with Zod validation
   const form = useForm<z.infer<typeof signInSchema>>({
@@ -56,11 +57,14 @@ export const SignInView = () => {
       {
         email: data.email,
         password: data.password,
+
+        callbackURL: "/", 
       },
       {
         onSuccess: () => {
           setPending(false);
-          router.push("/"); // Redirect to home on success
+          // Redirect to home page on success
+          router.push("/");
         },
         onError: ({ error }) => {
           setPending(false);
@@ -71,112 +75,162 @@ export const SignInView = () => {
     );
   };
 
+  // handle social sign-up (Google, GitHub)
+    const onSocial = (provider: "google" | "github") => {
+      setError(null); // Reset error state
+      setPending(true); // Set pending state to true
+      authClient.signIn.social(
+        {
+          provider: provider,
+          callbackURL:"/"
+        },
+        {
+          onSuccess: () => {
+            setPending(false); // Reset pending state
+           
+          },
+          onError: ({ error }) => {
+            setPending(false); // Reset pending state
+            console.error("Social sign up error:", error);
+            setError(error.message || "An error occurred during social sign up"); // Set error message
+          },
+        }
+      );
+    };
+  
+
   return (
     <div>
       {/* Main card container for the sign-in form */}
       <div className="flex flex-col gap-6 shadow-[0_20px_50px_rgba(255,200,221,_0.7)] w-full md:w-[500px] lg:w-[600px] xl:w-[700px] mx-auto my-10">
         <Card className="overflow-hidden  p-0">
           <CardContent className="grid p-0 md:grid-cols-2 xl:grid-cols-2">
-            {/* Sign-in form */}
-            <Form {...form}>
-              <form className="p-6 md:p-8 " onSubmit={form.handleSubmit(onSumit)}>
-                <div className="flex flex-col gap-6">
-                  {/* Welcome message */}
-                  <div className="flex flex-col items-center text-center">
-                    <h1 className="text-l font-bold text-[#ff4d6d]">
-                      Welcome back
-                    </h1>
-                    <p className="text-sm  text-balance text-muted-foreground">
-                      Login to your account
-                    </p>
-                  </div>
-                  {/* Email input field */}
-                  <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Email</FormLabel>
-                          <FormControl>
-                            <Input
-                              type="email"
-                              placeholder="Enter your email"
-                              {...field}
-                              className=" border-red-300 "
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  {/* Password input field */}
-                  <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Password</FormLabel>
-                          <FormControl>
-                            <Input
-                              type="password"
-                              placeholder="Enter your password"
-                              {...field}
-                              className=" border-red-300 "
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  {/* Error alert if sign-in fails */}
-                  {!!error && (
-                    <Alert className="bg-destructive/10 border-none">
-                      <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
-                      <AlertTitle>{error}</AlertTitle>
-                    </Alert>
-                  )}
+            <div>
+              {/* Sign-in form */}
+              <Form {...form}>
+                {/* off default behaviour of the form */}
+                <form
+                  className="p-6 md:p-6 md:pb-4 "
+                  onSubmit={form.handleSubmit(onSumit)}
+                >
+                  <div className="flex flex-col gap-6">
+                    {/* Welcome message */}
+                    <div className="flex flex-col items-center text-center">
+                      <h1 className="text-l font-bold text-[#ff4d6d]">
+                        Welcome back
+                      </h1>
+                      <p className="text-sm  text-balance text-muted-foreground">
+                        Login to your account
+                      </p>
+                    </div>
+                    {/* Email input field */}
+                    <div className="grid gap-3">
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                              <Input
+                                type="email"
+                                placeholder="Enter your email"
+                                {...field}
+                                className=" border-red-300 "
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    {/* Password input field */}
+                    <div className="grid gap-3">
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                              <Input
+                                type="password"
+                                placeholder="Enter your password"
+                                {...field}
+                                className=" border-red-300 "
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    {/* Error alert if sign-in fails */}
+                    {!!error && (
+                      <Alert className="bg-destructive/10 border-none">
+                        <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                        <AlertTitle>{error}</AlertTitle>
+                      </Alert>
+                    )}
 
-                  {/* Submit button */}
-                  <Button
-                    disabled={pending}
-                    type="submit"
-                    className="w-full bg-[#ff4d6d] hover:bg-[#ff4d6d]/90 text-white"
-                  >
-                    Sign In
-                  </Button>
-
-                  {/* Divider for alternative sign-in methods */}
-                  <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                    <span className="bg-card text-muted-foreground relative z-10 px-3">
-                      Or continue with
-                    </span>
-                  </div>
-                  {/* Social sign-in buttons (Google, GitHub) */}
-                  <div className="grid grid-cols-2 gap-4">
-                    <Button variant="outline" className="w-full" disabled={pending}>
-                      Google
-                    </Button>
-                    <Button variant="outline" className="w-full" disabled={pending}>
-                      GitHub
-                    </Button>
-                  </div>
-                  {/* Link to sign-up page */}
-                  <div className="text-sm text-center text-muted-foreground">
-                    Don't have an account?{" "}
-                    <Link
-                      href="/sign-up"
-                      className="text-[#ff4d6d] hover:underline"
+                    {/* Submit button */}
+                    <Button
+                      disabled={pending}
+                      type="submit"
+                      id="sign-in"
+                      className="w-full bg-[#ff4d6d] hover:bg-[#ff4d6d]/90 text-white"
                     >
-                      Sign Up
-                    </Link>
+                      Sign In
+                    </Button>
                   </div>
+                </form>
+              </Form>
+              <div className="flex flex-col gap-4 p-6 mt-0 md:p-6">
+                {/* Divider for alternative sign-in methods */}
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-3">
+                    Or continue with
+                  </span>
                 </div>
-              </form>
-            </Form>
+                {/* Social sign-in buttons (Google, GitHub) */}
+                <div className="grid grid-cols-2 gap-4">
+                  <Button
+                    variant="outline"
+                    className="w-full flex items-center justify-center"
+                    disabled={pending}
+                    onClick={(e) => {
+                      onSocial("google");
+                    }}
+                    
+                  >
+                    {/* <img src="/icon/google.png" alt="" className="relative w-5" />{"Google"}
+                     */}
+                     <FaGoogle></FaGoogle>
+                  </Button>
+                  <Button
+                    variant="outline"
+                    className="w-full flex items-center justify-center"
+                    disabled={pending}
+                    onClick={(e) => {
+                      onSocial("github");
+                    }}
+                  >
+                    <FaGithub></FaGithub>
+                    {/* <img src="/icon/github.png" alt="" className="relative w-6" />{"GitHub"} */}
+                  </Button>
+                </div>
+                {/* Link to sign-up page */}
+                <div className="text-sm text-center text-muted-foreground">
+                  Don't have an account?{" "}
+                  <Link
+                    href="/sign-up"
+                    className="text-[#ff4d6d] hover:underline"
+                  >
+                    Sign Up
+                  </Link>
+                </div>
+              </div>
+            </div>
 
             {/* Right side: Logo and branding (visible on md+ screens) */}
             <div className="relative hidden bg-gradient-to-r from-[#febdc9] via-[#fe899f] to-[#fe6f89]   md:flex flex-col gap-y-4 items-center justify-center">
