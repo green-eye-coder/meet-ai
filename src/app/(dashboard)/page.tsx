aa3893fdcc475f1dccec45054c4a509e4e1396f4
Hruthik chavan D <hruthikchavandsmg@gmail.com>
2025-06-13 12:13:41 +0530

    feat: integrate TRPC and React Query for improved data fetching and state management

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,18 +1,18 @@
-import {auth} from "@/lib/auth";
+import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-const Page= async ()=>{
-  const session=await auth.api.getSession({
+const Page = async () => {
+  const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if(!session){
+  if (!session) {
     redirect("/sign-in");
   }
 
-  return <HomeView/>
-}
+  return <HomeView />;
+};
 
 export default Page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Inter} from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/client";
 import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
-
 
 
 export const metadata: Metadata = {
@@ -20,12 +20,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} ${inter.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <TRPCReactProvider>
+      <html lang="en">
+        <body
+          className={`${inter.variable} ${inter.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </TRPCReactProvider>
   );
 }
